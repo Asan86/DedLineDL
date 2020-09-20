a98139ddd228eec0461399ba55d2868b9c47cf9a
Turdukulov Asan <asanturdukulov86@gmail.com>
2020-09-20 22:25:53 +0600

    DZ_2.3

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -1,47 +1,40 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class BankAccount {
-    double amount;
+    private double amount;
 
-    public double getAmount() {       //возвращяет текущий остаток на счете
-        return amount;
+
+    public BankAccount() {
     }
 
-    public double deposit() {         // положить деньги на счет
-        System.out.println("Вставьте суммму денег:");
-        Scanner sc = new Scanner(System.in);
-        double sum = sc.nextDouble();
-        System.out.println("вы пополнили счет на :" + sum);
-        return amount = amount + sum;
-    }
-
-    public double withDraw() {       //снимает указанную сумму со счета
-        System.out.println("укажите сумму которую хтите снять: ");
-        Scanner sc = new Scanner(System.in);
-        int sum = sc.nextInt();
-        System.out.println("вы сняли :" + sum);
-        if (amount > 0) {
-            amount = amount - sum;
-            if (amount < 0) {
-                amount = amount - amount;
-            }
-        }
-
-        if (amount < sum) {
-            try {
-                throw new LimitException("Лимит исчерпан", amount);
-            } catch (LimitException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println("остаток: " + amount);
-        return amount;
-    }
-
-    public String setAmount(double amount) {
+    public BankAccount(double amount) {
         this.amount = amount;
-        return null;
     }
+    public  double getAmount(){
+        return amount;
+
+
+    }
+    public double deposit(double sum){
+        amount = amount + sum;
+
+
+
+        System.out.println(" Ваш счет составляет: " + sum +  " Сом ");
+
+
+
+        return sum;
+    }
+    public void withDraw(int sum) throws LimitException {
+        if(amount < sum){
+            throw  new  LimitException(" Не достаточный баланс на счете ", getAmount());
+        }
+
+
+        amount = amount -sum;
+        System.out.println(" снято: " + sum);
+
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,27 @@
 package com.company;
 
 public class Main {
+    public static void main(String[] args) throws LimitException {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(20000);
+        for (int i = 0; ; i++) {
+            try {
+                bankAccount.withDraw(6000);
 
 
-    public static void main(String[] args) {
-        // write your code here
-        BankAccount bank = new BankAccount();
+            } catch (LimitException tt) {
+                System.out.println(" Сейчас " + bankAccount.getAmount());
+                bankAccount.withDraw((int) bankAccount.getAmount());
 
-        bank.deposit();
-        for (int i = 0;; i++) {
-            bank.withDraw();
+
+                System.out.println(tt.getMessage());
+
+                System.out.println(" Сейчас " + bankAccount.getAmount());
+                break;
+            }
+
         }
-
     }
-
 }
+
+
